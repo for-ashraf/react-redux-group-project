4c1a79e9b9ce7630c9d76839bb110307ed5eda5c
for-ashraf <46405183+for-ashraf@users.noreply.github.com>
2022-08-25 23:56:29 +0500

    project logic finalized

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/header';
+import Profile from './pages/Profile';
+import Rockets from './pages/Rockets';
+import Missions from './pages/Missions';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" exact element={<Rockets />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/profile" exact element={<Profile />} />
+      </Routes>
+    </Router>
   );
 }
 
